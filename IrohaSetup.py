970f7f0eb4655552ae5c52390796d18659ccce3a
BRM-Code <52327403+BRM-Code@users.noreply.github.com>
2023-04-12 20:08:36 +0100

    Removed a significant portion of unused/ needed code.
    Removed references to admin account as a hotspot should not be able to log in as an admin to the iroha instance.
    
    Added to the main file's listen_push_data to make it collect packets properly and push them to the blockchain.
    
    cleaned up the output by adding a debug mode and modifying any print statements.

--- IrohaSetup.py
+++ IrohaSetup.py
@@ -5,20 +5,19 @@ import grpc
 from utilities.errorCodes2Hr import get_proper_functions_for_commands
 import os
 from datetime import datetime
-from iroha import Iroha, IrohaGrpc, IrohaCrypto, primitive_pb2
+from iroha import Iroha, IrohaGrpc, IrohaCrypto
 
-IROHA_HOST_ADDR = 'localhost'
+IROHA_HOST_ADDR = '192.168.0.199'
 IROHA_PORT = '50051'
 IROHA_DOMAIN = "test"
-ADMIN_ACCOUNT_ID = f'admin@{IROHA_DOMAIN}'
-iroha_admin = Iroha(ADMIN_ACCOUNT_ID)
+DEBUG = False
 
 
 def send_transaction_and_print_status(transaction):
     hex_hash = binascii.hexlify(IrohaCrypto.hash(transaction))
     creator_id = transaction.payload.reduced_payload.creator_account_id
     commands = get_commands_from_tx(transaction)
-    print(f'[{creator_id}] Transaction "{commands}", hash = {hex_hash}')
+    print(f'[{creator_id}] Transaction "{commands}", hash = {hex_hash}') if DEBUG else None
     try:
         net.send_tx(transaction)
         time_start = time.perf_counter()
@@ -30,16 +29,17 @@ def send_transaction_and_print_status(transaction):
         if status_name == "STATELESS_VALIDATION_SUCCESS":
             print(f"[{creator_id}] Transaction validated at {time.perf_counter() - time_start}")
         if status_name == "COMMITTED":
-            time_end = time.perf_counter() - time_start
-            print(f"[{creator_id}] Transaction took: {time_end}")
+            time_end = round((time.perf_counter() - time_start) * 1000) / 1000
+            print(f"[{creator_id}] Transaction took: {time_end} seconds")
             return
-        print(f"    {i}: status_name={status_name}, status_code={status_code}, "
-              f"error_code={error_code}")
+
         if status_name in ('STATEFUL_VALIDATION_FAILED', 'STATELESS_VALIDATION_FAILED', 'REJECTED'):
             error_code_hr = get_proper_functions_for_commands(commands)(error_code)
             raise RuntimeError(f"{status_name} failed on tx: "
                                f"{transaction} due to reason {error_code}: "
                                f"{error_code_hr}")
+
+        print(f"    {i}: Status={status_name} SC={status_code} EC={error_code}") if DEBUG else None
 
 
 def get_commands_from_tx(transaction):
@@ -53,18 +53,18 @@ def get_commands_from_tx(transaction):
 # This function generates a private/public key pair using the IrohaCrypto module.
 # If the key files already exist, it reads the values from them.
 def get_keys(account_id):
-    print(f"[{account_id}] Looking for keys -> ", end="")
+    print(f"[{account_id}] Looking for keys -> ", end="") if DEBUG else None
     private_key_file = f'Keys/{account_id}@{IROHA_DOMAIN}.priv'
     public_key_file = f'Keys/{account_id}@{IROHA_DOMAIN}.pub'
 
     if os.path.exists(private_key_file) and os.path.exists(public_key_file):
-        print("Found keys")
+        print("Found keys") if DEBUG else None
         with open(private_key_file, 'rb') as f:
             private_key = f.read()
         with open(public_key_file, 'rb') as f:
             public_key = f.read()
     else:
-        print("Generating new keys")
+        print("Generating new keys") if DEBUG else None
         private_key = IrohaCrypto.private_key()
         public_key = IrohaCrypto.derive_public_key(private_key)
         with open(private_key_file, 'wb') as f:
@@ -74,24 +74,8 @@ def get_keys(account_id):
     return private_key, public_key
 
 
-def create_account(device_id):
-    print(f"[{device_id}] Checking account -> ", end="")
-    priv, pub = get_keys(device_id)
-    tx = iroha_admin.transaction([
-        iroha_admin.command('CreateAccount', account_name=device_id, domain_id=IROHA_DOMAIN,
-                            public_key=pub)
-    ])
-    IrohaCrypto.sign_transaction(tx, ADMIN_PRIVATE_KEY)
-    try:
-        send_transaction_and_print_status(tx)
-        print("Created account for device")
-    except RuntimeError:
-        print("Account already exists")
-        return
-
-
 def store_telemetry_data(telemetry, drone_id, gateway_id):
-    print(f"[{gateway_id}] is storing info from {drone_id}")
+    print(f"[{gateway_id}@{IROHA_DOMAIN}] is storing info from {drone_id}")
     telemetry['timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     priv, pub = get_keys(gateway_id)
     iroha_gateway = Iroha(f"{gateway_id}@{IROHA_DOMAIN}")
@@ -106,32 +90,15 @@ def store_telemetry_data(telemetry, drone_id, gateway_id):
     return
 
 
-def create_domain(default_role='user'):
-    print(f"Creating Domain : {IROHA_DOMAIN}")
-    commands = [iroha_admin.command('CreateDomain', domain_id=IROHA_DOMAIN, default_role=default_role), ]
-    tx = IrohaCrypto.sign_transaction(iroha_admin.transaction(commands), ADMIN_PRIVATE_KEY)
-    send_transaction_and_print_status(tx)
-
-
-def uav_allow_gateway(drone_id, gateway_id):
-    print(f"[{drone_id}] is allowing details from {gateway_id}")
-    priv, pub = get_keys(drone_id)
-    tx = iroha_admin.transaction([
-        iroha_admin.command('GrantPermission', account_id=f'{gateway_id}@{IROHA_DOMAIN}',
-                            permission=primitive_pb2.can_set_my_account_detail)
-    ], creator_account=f"{drone_id}@{IROHA_DOMAIN}")
-    IrohaCrypto.sign_transaction(tx, priv)
-    send_transaction_and_print_status(tx)
-
-
-def get_device_details(device_id):
+def get_device_details(device_id, gateway_id):
     print(f"[{device_id}] Looking for details -> ", end="")
-    query = iroha_admin.query('GetAccountDetail', account_id=f"{device_id}@{IROHA_DOMAIN}")
-    IrohaCrypto.sign_query(query, ADMIN_PRIVATE_KEY)
+    iroha_gateway = Iroha(f"{gateway_id}@{IROHA_DOMAIN}")
+    priv, pub = get_keys(gateway_id)
+    query = iroha_gateway.query('GetAccountDetail', account_id=f"{device_id}@{IROHA_DOMAIN}")
+    IrohaCrypto.sign_query(query, priv)
     response = net.send_query(query)
     detail = response.account_detail_response.detail
     print(detail) if detail else print("No Details!")
 
 
 net = IrohaGrpc(f"{IROHA_HOST_ADDR}:{IROHA_PORT}")
-ADMIN_PRIVATE_KEY, ADMIN_PUB_KEY = get_keys("admin")
--- main.py
+++ main.py
@@ -1,50 +1,83 @@
+import base64
 import json
-import time
 import socket
-from IrohaSetup import store_telemetry_data, get_device_details
-from TelemetryFaker import generate_telemetry, convert_12bit
+import sys
+from IrohaSetup import store_telemetry_data, get_device_details, DEBUG
 
 
 def listen_push_data():
-    print("Connecting to the packet forwarder...")
+    print("[System] Connecting to the packet forwarder...", end="")
     hotspot_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    hotspot_socket.bind(('localhost', 1700))
+    hotspot_socket.bind(('localhost', 1730))
+    print("Connected!")
 
     while True:
         data, address = hotspot_socket.recvfrom(1024)
         # check if the received packet is a PUSH_DATA packet
         if data[3] == 0x00:
-            print("Received a packet!")
 
-            json_str = data[12:].decode('utf-8')
-            try:
-                json_obj = json.loads(json_str)
-                print(json_obj)
-            except json.JSONDecodeError:
-                print('Error: Invalid JSON')
-
+            # Tell the packet-forwarder data arrived
             token = data[1:3]
             push_ack = bytes([2, token[0], token[1], 0x01])
             hotspot_socket.sendto(push_ack, address)
 
+            print("[PK] Received a packet -> ", end="")
+            json_str = data[12:].decode('utf-8')
+            try:
+                packet = json.loads(json_str)
+                if 'stat' in packet:
+                    print("stat packet!")
+                elif 'rxpk' in packet:
+                    print("transmission packet!")
+                    data_decoded = base64.b64decode(packet['rxpk'][0]['data'])
+                    print(f"Data extracted: {data_decoded}") if DEBUG else None
+                    uav_id, tele = process_telemetry(data_decoded)
+                    store_telemetry_data(tele, uav_id, gateway_id)
+                    get_device_details(uav_id, gateway_id) if DEBUG else None
+                else:
+                    print("Error: Unknown packet type!")
+            except json.JSONDecodeError:
+                print('Error: Invalid JSON received!')
 
-drone_id = "u1"
+
+def process_telemetry(decoded_data):
+    data_list = str(decoded_data[1:len(decoded_data)]).split(',')
+    print(f"Data Decoded = {data_list}") if DEBUG else None
+    uav_id = data_list[12][0:2]
+    telemetry = {
+        'Latitude': data_list[0][2:len(data_list[0])],
+        'Longitude': data_list[1],
+        'GroundSpeed': data_list[4],
+        'Altitude': data_list[3],
+        'Satellites': data_list[5],
+        'SatFix': bool(data_list[11][0]),
+        'Pitch': data_list[8],
+        'Roll': data_list[9],
+        'Heading': data_list[10],
+        'Vbatt': data_list[2],
+        'Consumption': data_list[6],
+        'RSSI': data_list[7],
+        'arm': bool(data_list[11][1])
+    }
+    print(f"[{uav_id}] Telemetry: {telemetry}") if DEBUG else None
+    return uav_id, telemetry
+
+
+icon = ("     __                      __    _       __         __  __      __                   __ \n"
+        "    / /   ____  ____  ____ _/ /   (_)___  / /__      / / / /___  / /__________  ____  / /_\n"
+        "   / /   / __ \/ __ \/ __ `/ /   / / __ \/ //_/_____/ /_/ / __ \/ __/ ___/ __ \/ __ \/ __/\n"
+        "  / /___/ /_/ / / / / /_/ / /___/ / / / / ,< /_____/ __  / /_/ / /_(__  ) /_/ / /_/ / /_  \n"
+        " /_____/\____/_/ /_/\__, /_____/_/_/ /_/_/|_|     /_/ /_/\____/\__/____/ .___/\____/\__/  \n"
+        "                   /____/                                             /_/             ")
+print(icon)
+
+if len(sys.argv) > 1 and not sys.argv[1]:
+    print(f"[System] Debug: ON")
+    DEBUG = sys.argv[1]
+else:
+    print(f"[System] Debug: OFF")
 gateway_id = "g1"
-
-# create_domain("test1")
-# drone_account = create_account(drone_id)
-# gateway_account = create_account(gateway_id)
-
-# grant_permission(drone_id)
-# uav_allow_gateway(drone_id, gateway_id)
-
-tele = None
+print(f"[System] Gateway ID: {gateway_id}")
 
 while True:
-    for i in range(10):
-        get_device_details(drone_id)
-        print(f"[{i}] Sending message")
-        tele = generate_telemetry(tele)  # Will be replaced with actual telemetry from a UAV
-        bit_tele = convert_12bit(tele)
-        store_telemetry_data(bit_tele, drone_id, gateway_id)
-        time.sleep(5)
+    listen_push_data()
